b4fec502fa37ece1f956d283c0d7f7d422941c79
niallowain <78670002+niallowain@users.noreply.github.com>
2021-03-07 11:24:19 +0000

    Update data_processing.py

--- Hurling/data_processing.py
+++ Hurling/data_processing.py
@@ -5,4 +5,16 @@ def pass_process(passes, exceptions)
 
     char_count = 0
     plays = len(passes)
-    for char in exceptio
+    for i in range(0,plays):
+        char_curr = passes[i]
+        if(char_curr in exceptions):
+            char_count = char_count + 1
+        else:
+            char_count = char_count
+    
+    plays_array = np.array(char_count)
+    
+    for i in range(0,plays)
+        j = 0
+        while(passes[i] not in exceptions):
+            
